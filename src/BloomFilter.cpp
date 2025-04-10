c763966600e513e4ed335f097bb0414aa890d46f
itaysmouha <itaysmouha@gmail.com>
2025-04-10 19:48:20 +0300

    commented out hash function in URL

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -16,7 +16,7 @@ public:
 
     std::string getURL() const; //getURL function
 
-    std::vector<size_t> hash(const std::vector<std::hash<std::string>>& hash_functions) const; //hash function
+    // std::vector<size_t> hash(const std::vector<std::hash<std::string>>& hash_functions) const; //hash function
 
     bool operator==(const URL& compURL) const; //Creating == operator to check if 2 URL are the same
 
@@ -30,15 +30,17 @@ std::string URL::getURL() const {
 }
 
 // Compute hashes for the URL using multiple hash functions
-std::vector<size_t> URL::hash(const std::vector<std::hash<std::string>>& hash_functions) const {
-    std::vector<size_t> hash_values;
-    for (const auto& hash_function : hash_functions) {
-        hash_values.push_back(hash_function(url));
-    }
-    return hash_values;
-}
+// std::vector<size_t> URL::hash(const std::vector<std::hash<std::string>>& hash_functions) const {
+//     std::vector<size_t> hash_values;
+//     for (const auto& hash_function : hash_functions) {
+//         hash_values.push_back(hash_function(url));
+//     }
+//     return hash_values;
+// }
 
 bool URL::operator==(const URL& other) const {
     return url == other.url;
 }
+
 #endif
+
