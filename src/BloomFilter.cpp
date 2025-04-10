356b55cd7fc15d75ffe5117ca5296f3632016446
Yuvaltar <yuvaltar1337@gmail.com>
2025-04-10 19:03:48 +0300

    uploding yuvals b

--- src/BloomFilter.cpp
+++ src/BloomFilter.cpp
@@ -1,1 +1,33 @@
 // bloomfilter.cpp file
+#include "BloomFilter.h"
+
+BloomFilter::BloomFilter(size_t size, const std::vector<std::shared_ptr<IHashFunction>>& hashFunctions)
+    : m_size(size), bitArray(size, false), hashFunctions(hashFunctions) {}
+
+void BloomFilter::add(const std::string& item) {
+    for (const auto& hf : hashFunctions) {
+        size_t hashValue = hf->hash(item);
+        size_t index = hashValue % m_size;
+        bitArray[index] = true;
+    }
+}
+
+bool BloomFilter::possiblyContains(const std::string& item) const {
+    for (const auto& hf : hashFunctions) {
+        size_t hashValue = hf->hash(item);
+        size_t index = hashValue % m_size;
+        if (!bitArray[index])
+            return false;
+    }
+    return true;
+}
+
+const std::vector<bool>& BloomFilter::getBitArray() const {
+    return bitArray;
+}
+
+void BloomFilter::setBitArray(const std::vector<bool>& bits) {
+    if(bits.size() == m_size) {
+        bitArray = bits;
+    }
+}
--- src/BloomFilter.h
+++ src/BloomFilter.h
@@ -1,1 +1,31 @@
 //header file
+#ifndef BLOOMFILTER_H
+#define BLOOMFILTER_H
+
+#include <vector>
+#include <memory>
+#include <string>
+#include "IHashFunction.h"
+
+class BloomFilter {
+public:
+    BloomFilter(size_t size, const std::vector<std::shared_ptr<IHashFunction>>& hashFunctions); // constructor
+    
+    // Adds an item (e.g., a URL) to the filter.
+    void add(const std::string& item);
+    
+    // Returns true if, according to the bloom filter, the item might be present.
+    // If any bit is not set, then returns false.
+    bool possiblyContains(const std::string& item) const;
+    
+    // For persistence: get and set the internal bit array.
+    const std::vector<bool>& getBitArray() const;
+    void setBitArray(const std::vector<bool>& bits);
+    
+private:
+    size_t m_size;
+    std::vector<bool> bitArray;
+    std::vector<std::shared_ptr<IHashFunction>> hashFunctions;
+};
+
+#endif // BLOOMFILTER_H
