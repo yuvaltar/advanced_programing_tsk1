20df033c55b23322aa5860991d40e4b47718c27f
itaysmouha <itaysmouha@gmail.com>
2025-04-11 12:09:51 +0300

    commit

--- src/url.cpp
+++ src/url.cpp
@@ -25,14 +25,7 @@ std::string URL::getURL() const {
     return url;
 }
 
-// Compute hashes for the URL using multiple hash functions
-// std::vector<size_t> URL::hash(const std::vector<std::hash<std::string>>& hash_functions) const {
-//     std::vector<size_t> hash_values;
-//     for (const auto& hash_function : hash_functions) {
-//         hash_values.push_back(hash_function(url));
-//     }
-//     return hash_values;
-// }
+
 
 bool URL::operator==(const URL& other) const {
     return url == other.url;
